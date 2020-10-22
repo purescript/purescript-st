86d45f02f2ed196d5ce720b340b5a4c78c030109
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-22 12:26:51 -0700

    Fix compiler error due to Region using old kind syntax

--- src/Control/Monad/ST/Internal.purs
+++ src/Control/Monad/ST/Internal.purs
@@ -21,7 +21,7 @@ import Partial.Unsafe (unsafePartial)
 -- | `ST` is concerned with _restricted_ mutation. Mutation is restricted to a
 -- | _region_ of mutable references. This kind is inhabited by phantom types
 -- | which represent regions in the type system.
-foreign import kind Region
+foreign import data Region :: Type
 
 -- | The `ST` type constructor allows _local mutation_, i.e. mutation which
 -- | does not "escape" into the surrounding computation.
