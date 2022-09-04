25e166bd208a091050ba87c7540c3fc75cb61978
Mike Solomon <mike120982@gmail.com>
2022-09-04 17:20:25 +0300

    Runs eslint

--- src/Control/Monad/ST/Uncurried.js
+++ src/Control/Monad/ST/Uncurried.js
@@ -1,115 +1,132 @@
 export const mkSTFn1 = function mkSTFn1(fn) {
-    return function(x) {
-      return fn(x)();
+  return function(x) {
+    return fn(x)();
+  };
+};
+  
+export const mkSTFn2 = function mkSTFn2(fn) {
+  return function(a, b) {
+    return fn(a)(b)();
+  };
+};
+  
+export const mkSTFn3 = function mkSTFn3(fn) {
+  return function(a, b, c) {
+    return fn(a)(b)(c)();
+  };
+};
+  
+export const mkSTFn4 = function mkSTFn4(fn) {
+  return function(a, b, c, d) {
+    return fn(a)(b)(c)(d)();
+  };
+};
+  
+export const mkSTFn5 = function mkSTFn5(fn) {
+  return function(a, b, c, d, e) {
+    return fn(a)(b)(c)(d)(e)();
+  };
+};
+  
+export const mkSTFn6 = function mkSTFn6(fn) {
+  return function(a, b, c, d, e, f) {
+    return fn(a)(b)(c)(d)(e)(f)();
+  };
+};
+  
+export const mkSTFn7 = function mkSTFn7(fn) {
+  return function(a, b, c, d, e, f, g) {
+    return fn(a)(b)(c)(d)(e)(f)(g)();
+  };
+};
+  
+export const mkSTFn8 = function mkSTFn8(fn) {
+  return function(a, b, c, d, e, f, g, h) {
+    return fn(a)(b)(c)(d)(e)(f)(g)(h)();
+  };
+};
+  
+export const mkSTFn9 = function mkSTFn9(fn) {
+  return function(a, b, c, d, e, f, g, h, i) {
+    return fn(a)(b)(c)(d)(e)(f)(g)(h)(i)();
+  };
+};
+  
+export const mkSTFn10 = function mkSTFn10(fn) {
+  return function(a, b, c, d, e, f, g, h, i, j) {
+    return fn(a)(b)(c)(d)(e)(f)(g)(h)(i)(j)();
+  };
+};
+  
+export const runSTFn1 = function runSTFn1(fn) {
+  return function(a) {
+    return function() {
+      return fn(a);
     };
   };
+};
   
-  export const mkSTFn2 = function mkSTFn2(fn) {
-    return function(a, b) {
-      return fn(a)(b)();
-    };
-  };
-  
-  export const mkSTFn3 = function mkSTFn3(fn) {
-    return function(a, b, c) {
-      return fn(a)(b)(c)();
-    };
-  };
-  
-  export const mkSTFn4 = function mkSTFn4(fn) {
-    return function(a, b, c, d) {
-      return fn(a)(b)(c)(d)();
-    };
-  };
-  
-  export const mkSTFn5 = function mkSTFn5(fn) {
-    return function(a, b, c, d, e) {
-      return fn(a)(b)(c)(d)(e)();
-    };
-  };
-  
-  export const mkSTFn6 = function mkSTFn6(fn) {
-    return function(a, b, c, d, e, f) {
-      return fn(a)(b)(c)(d)(e)(f)();
-    };
-  };
-  
-  export const mkSTFn7 = function mkSTFn7(fn) {
-    return function(a, b, c, d, e, f, g) {
-      return fn(a)(b)(c)(d)(e)(f)(g)();
-    };
-  };
-  
-  export const mkSTFn8 = function mkSTFn8(fn) {
-    return function(a, b, c, d, e, f, g, h) {
-      return fn(a)(b)(c)(d)(e)(f)(g)(h)();
-    };
-  };
-  
-  export const mkSTFn9 = function mkSTFn9(fn) {
-    return function(a, b, c, d, e, f, g, h, i) {
-      return fn(a)(b)(c)(d)(e)(f)(g)(h)(i)();
-    };
-  };
-  
-  export const mkSTFn10 = function mkSTFn10(fn) {
-    return function(a, b, c, d, e, f, g, h, i, j) {
-      return fn(a)(b)(c)(d)(e)(f)(g)(h)(i)(j)();
-    };
-  };
-  
-  export const runSTFn1 = function runSTFn1(fn) {
-    return function(a) {
+export const runSTFn2 = function runSTFn2(fn) {
+  return function(a) {
+    return function(b) {
       return function() {
-        return fn(a);
+        return fn(a, b);
       };
     };
   };
+};
   
-  export const runSTFn2 = function runSTFn2(fn) {
-    return function(a) {
-      return function(b) {
+export const runSTFn3 = function runSTFn3(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
         return function() {
-          return fn(a, b);
+          return fn(a, b, c);
         };
       };
     };
   };
+};
   
-  export const runSTFn3 = function runSTFn3(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
+export const runSTFn4 = function runSTFn4(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
           return function() {
-            return fn(a, b, c);
+            return fn(a, b, c, d);
           };
         };
       };
     };
   };
+};
   
-  export const runSTFn4 = function runSTFn4(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
+export const runSTFn5 = function runSTFn5(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
             return function() {
-              return fn(a, b, c, d);
+              return fn(a, b, c, d, e);
             };
           };
         };
       };
     };
   };
+};
   
-  export const runSTFn5 = function runSTFn5(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
+export const runSTFn6 = function runSTFn6(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
+            return function(f) {
               return function() {
-                return fn(a, b, c, d, e);
+                return fn(a, b, c, d, e, f);
               };
             };
           };
@@ -117,16 +134,18 @@ export const mkSTFn1 = function mkSTFn1(fn) {
       };
     };
   };
+};
   
-  export const runSTFn6 = function runSTFn6(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
-              return function(f) {
+export const runSTFn7 = function runSTFn7(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
+            return function(f) {
+              return function(g) {
                 return function() {
-                  return fn(a, b, c, d, e, f);
+                  return fn(a, b, c, d, e, f, g);
                 };
               };
             };
@@ -135,17 +154,19 @@ export const mkSTFn1 = function mkSTFn1(fn) {
       };
     };
   };
+};
   
-  export const runSTFn7 = function runSTFn7(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
-              return function(f) {
-                return function(g) {
+export const runSTFn8 = function runSTFn8(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
+            return function(f) {
+              return function(g) {
+                return function(h) {
                   return function() {
-                    return fn(a, b, c, d, e, f, g);
+                    return fn(a, b, c, d, e, f, g, h);
                   };
                 };
               };
@@ -155,18 +176,20 @@ export const mkSTFn1 = function mkSTFn1(fn) {
       };
     };
   };
+};
   
-  export const runSTFn8 = function runSTFn8(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
-              return function(f) {
-                return function(g) {
-                  return function(h) {
+export const runSTFn9 = function runSTFn9(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
+            return function(f) {
+              return function(g) {
+                return function(h) {
+                  return function(i) {
                     return function() {
-                      return fn(a, b, c, d, e, f, g, h);
+                      return fn(a, b, c, d, e, f, g, h, i);
                     };
                   };
                 };
@@ -177,19 +200,21 @@ export const mkSTFn1 = function mkSTFn1(fn) {
       };
     };
   };
+};
   
-  export const runSTFn9 = function runSTFn9(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
-              return function(f) {
-                return function(g) {
-                  return function(h) {
-                    return function(i) {
+export const runSTFn10 = function runSTFn10(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return function(d) {
+          return function(e) {
+            return function(f) {
+              return function(g) {
+                return function(h) {
+                  return function(i) {
+                    return function(j) {
                       return function() {
-                        return fn(a, b, c, d, e, f, g, h, i);
+                        return fn(a, b, c, d, e, f, g, h, i, j);
                       };
                     };
                   };
@@ -201,29 +226,4 @@ export const mkSTFn1 = function mkSTFn1(fn) {
       };
     };
   };
-  
-  export const runSTFn10 = function runSTFn10(fn) {
-    return function(a) {
-      return function(b) {
-        return function(c) {
-          return function(d) {
-            return function(e) {
-              return function(f) {
-                return function(g) {
-                  return function(h) {
-                    return function(i) {
-                      return function(j) {
-                        return function() {
-                          return fn(a, b, c, d, e, f, g, h, i, j);
-                        };
-                      };
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+};
