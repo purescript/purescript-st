2cc2f8c1ed9f57725f546d902c8ef520a4f79bce
Mike Solomon <mike120982@gmail.com>
2022-09-25 14:33:03 +0300

    Changes placement of nominal type variable

--- src/Control/Monad/ST/Uncurried.purs
+++ src/Control/Monad/ST/Uncurried.purs
@@ -18,84 +18,84 @@ module Control.Monad.ST.Uncurried where
 
 import Control.Monad.ST.Internal (ST, Region)
 
-foreign import data STFn1 :: Region -> Type -> Type -> Type
+foreign import data STFn1 :: Type -> Type -> Region -> Type
 
-type role STFn1 nominal representational representational
+type role STFn1 representational nominal representational
 
-foreign import data STFn2 :: Region -> Type -> Type -> Type -> Type
+foreign import data STFn2 :: Type -> Type -> Type -> Region -> Type
 
-type role STFn2 nominal representational representational representational
+type role STFn2 representational representational nominal representational
 
-foreign import data STFn3 :: Region -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn3 :: Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn3 nominal representational representational representational representational
+type role STFn3 representational representational representational nominal representational
 
-foreign import data STFn4 :: Region -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn4 :: Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn4 nominal representational representational representational representational representational
+type role STFn4 representational representational representational representational nominal representational
 
-foreign import data STFn5 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn5 :: Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn5 nominal representational representational representational representational representational representational
+type role STFn5 representational representational representational representational representational nominal representational
 
-foreign import data STFn6 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn6 :: Type -> Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn6 nominal representational representational representational representational representational representational representational
+type role STFn6 representational representational representational representational representational representational nominal representational
 
-foreign import data STFn7 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn7 :: Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn7 nominal representational representational representational representational representational representational representational representational
+type role STFn7 representational representational representational representational representational representational representational nominal representational
 
-foreign import data STFn8 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn8 :: Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn8 nominal representational representational representational representational representational representational representational representational representational
+type role STFn8 representational representational representational representational representational representational representational representational nominal representational
 
-foreign import data STFn9 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn9 :: Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn9 nominal representational representational representational representational representational representational representational representational representational representational
+type role STFn9 representational representational representational representational representational representational representational representational representational nominal representational
 
-foreign import data STFn10 :: Region -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type
+foreign import data STFn10 :: Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Type -> Region -> Type
 
-type role STFn10 nominal representational representational representational representational representational representational representational representational representational representational representational
+type role STFn10 representational representational representational representational representational representational representational representational representational representational nominal representational
 
-foreign import mkSTFn1 :: forall t a r.
-  (a -> ST t r) -> STFn1 t a r
-foreign import mkSTFn2 :: forall t a b r.
-  (a -> b -> ST t r) -> STFn2 t a b r
-foreign import mkSTFn3 :: forall t a b c r.
-  (a -> b -> c -> ST t r) -> STFn3 t a b c r
-foreign import mkSTFn4 :: forall t a b c d r.
-  (a -> b -> c -> d -> ST t r) -> STFn4 t a b c d r
-foreign import mkSTFn5 :: forall t a b c d e r.
-  (a -> b -> c -> d -> e -> ST t r) -> STFn5 t a b c d e r
-foreign import mkSTFn6 :: forall t a b c d e f r.
-  (a -> b -> c -> d -> e -> f -> ST t r) -> STFn6 t a b c d e f r
-foreign import mkSTFn7 :: forall t a b c d e f g r.
-  (a -> b -> c -> d -> e -> f -> g -> ST t r) -> STFn7 t a b c d e f g r
-foreign import mkSTFn8 :: forall t a b c d e f g h r.
-  (a -> b -> c -> d -> e -> f -> g -> h -> ST t r) -> STFn8 t a b c d e f g h r
-foreign import mkSTFn9 :: forall t a b c d e f g h i r.
-  (a -> b -> c -> d -> e -> f -> g -> h -> i -> ST t r) -> STFn9 t a b c d e f g h i r
-foreign import mkSTFn10 :: forall t a b c d e f g h i j r.
-  (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> ST t r) -> STFn10 t a b c d e f g h i j r
+foreign import mkSTFn1 :: forall a t r.
+  (a -> ST t r) -> STFn1 a t r
+foreign import mkSTFn2 :: forall a b t r.
+  (a -> b -> ST t r) -> STFn2 a b t r
+foreign import mkSTFn3 :: forall a b c t r.
+  (a -> b -> c -> ST t r) -> STFn3 a b c t r
+foreign import mkSTFn4 :: forall a b c d t t r.
+  (a -> b -> c -> d -> ST t r) -> STFn4 a b c d t t r
+foreign import mkSTFn5 :: forall a b c d e t r.
+  (a -> b -> c -> d -> e -> ST t r) -> STFn5 a b c d e t r
+foreign import mkSTFn6 :: forall a b c d e f t r.
+  (a -> b -> c -> d -> e -> f -> ST t r) -> STFn6 a b c d e f t r
+foreign import mkSTFn7 :: forall a b c d e f g t r.
+  (a -> b -> c -> d -> e -> f -> g -> ST t r) -> STFn7 a b c d e f g t t r
+foreign import mkSTFn8 :: forall a b c d e f g h t r.
+  (a -> b -> c -> d -> e -> f -> g -> h -> ST t r) -> STFn8 a b c d e f g h t r
+foreign import mkSTFn9 :: forall a b c d e f g h i t r.
+  (a -> b -> c -> d -> e -> f -> g -> h -> i -> ST t r) -> STFn9 a b c d e f g h i t r
+foreign import mkSTFn10 :: forall a b c d e f g h i j t r.
+  (a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> ST t r) -> STFn10 a b c d e f g h i j t r
 
-foreign import runSTFn1 :: forall t a r.
-  STFn1 t a r -> a -> ST t r
-foreign import runSTFn2 :: forall t a b r.
-  STFn2 t a b r -> a -> b -> ST t r
-foreign import runSTFn3 :: forall t a b c r.
-  STFn3 t a b c r -> a -> b -> c -> ST t r
-foreign import runSTFn4 :: forall t a b c d r.
-  STFn4 t a b c d r -> a -> b -> c -> d -> ST t r
-foreign import runSTFn5 :: forall t a b c d e r.
-  STFn5 t a b c d e r -> a -> b -> c -> d -> e -> ST t r
-foreign import runSTFn6 :: forall t a b c d e f r.
-  STFn6 t a b c d e f r -> a -> b -> c -> d -> e -> f -> ST t r
-foreign import runSTFn7 :: forall t a b c d e f g r.
-  STFn7 t a b c d e f g r -> a -> b -> c -> d -> e -> f -> g -> ST t r
-foreign import runSTFn8 :: forall t a b c d e f g h r.
-  STFn8 t a b c d e f g h r -> a -> b -> c -> d -> e -> f -> g -> h -> ST t r
-foreign import runSTFn9 :: forall t a b c d e f g h i r.
-  STFn9 t a b c d e f g h i r -> a -> b -> c -> d -> e -> f -> g -> h -> i -> ST t r
-foreign import runSTFn10 :: forall t a b c d e f g h i j r.
-  STFn10 t a b c d e f g h i j r -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> ST t r
+foreign import runSTFn1 :: forall a t r.
+  STFn1 a t r -> a -> ST t r
+foreign import runSTFn2 :: forall a b t r.
+  STFn2 a b t r -> a -> b -> ST t r
+foreign import runSTFn3 :: forall a b c t r.
+  STFn3 a b c t r -> a -> b -> c -> ST t r
+foreign import runSTFn4 :: forall a b c d t r.
+  STFn4 a b c d t r -> a -> b -> c -> d -> ST t r
+foreign import runSTFn5 :: forall a b c d e t r.
+  STFn5 a b c d e t r -> a -> b -> c -> d -> e -> ST t r
+foreign import runSTFn6 :: forall a b c d e f t r.
+  STFn6 a b c d e f t r -> a -> b -> c -> d -> e -> f -> ST t r
+foreign import runSTFn7 :: forall a b c d e f g t r.
+  STFn7 a b c d e f g t r -> a -> b -> c -> d -> e -> f -> g -> ST t r
+foreign import runSTFn8 :: forall a b c d e f g h t r.
+  STFn8 a b c d e f g h t r -> a -> b -> c -> d -> e -> f -> g -> h -> ST t r
+foreign import runSTFn9 :: forall a b c d e f g h i t r.
+  STFn9 a b c d e f g h i t r -> a -> b -> c -> d -> e -> f -> g -> h -> i -> ST t r
+foreign import runSTFn10 :: forall a b c d e f g h i j t r.
+  STFn10 a b c d e f g h i j t r -> a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> ST t r
